43341b402863e42066678f573c3d06f7b4db9698
chenyuzou <chenyuzou@qq.com>
2021-07-13 11:42:11 +0800

    .

--- app/gateway/api/internal/svc/servicecontext.go
+++ app/gateway/api/internal/svc/servicecontext.go
@@ -2,14 +2,17 @@ package svc
 
 import (
 	"demo/app/gateway/api/internal/config"
+	"github.com/tal-tech/go-queue/dq"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config   config.Config
+	Producer dq.Producer
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:   c,
+		Producer: dq.NewProducer(c.DqConf.Beanstalks),
 	}
 }
